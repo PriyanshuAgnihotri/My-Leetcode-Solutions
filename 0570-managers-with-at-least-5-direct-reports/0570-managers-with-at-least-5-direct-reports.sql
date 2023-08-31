148ebeaef61736f2c70d6fa19b1c5ae6e5567210
PriyanshuAgnihotri <98649873+PriyanshuAgnihotri@users.noreply.github.com>
2023-08-31 11:23:22 +0530

    Time: 552 ms (79.63%), Space: 0B (100%) - LeetHub

---
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+select c.name from (select a.name, count(a.name) countname
+from Employee as a left join Employee as b
+on a.id=b.managerid
+group by a.name,a.id) as c where c.countname>4;
